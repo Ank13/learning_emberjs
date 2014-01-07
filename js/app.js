09371a3f4b12d37890a9651b94ee9be302a3c887
ank13 <avi.kaufman@gmail.com>
2014-01-07 14:03:34 -0500

    adds routes

--- js/app.js
+++ js/app.js
@@ -3,3 +3,12 @@ var App = Ember.Application.create({
   // log out message to browser for every new page
   LOG_TRANSITIONS: true
 });
+
+// Router
+App.Router.map(function(){
+  // when browser visits path /about, use route 'about' and load about template into outlet
+  this.route('about');
+    // optionally, can specify another path if not named 'about'
+    // this.route('about', { path 'aboutus'} );
+
+});
